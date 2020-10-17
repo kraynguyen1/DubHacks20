8d4f664c97edbdda6812c5a997a73e356738fae6
JongSeok Jung <jong03720@gmail.com>
2020-10-17 14:07:16 -0700

    file adjustment
    adding main.py and removing Discord.py (testing fiel)

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import discord
+from discord.ext import commands
+from discord.utils import get
+
+CLIENT = commands.Bot(command_prefix='/')
+TOKEN = 'NzY3MTIzNzY1Mjg3OTc2OTgw.X4tVrg.76zyxJjTYwMAmeVnYLJxzGKGbq0'
+
+
+# When you type /ping
+# the bot will say "pong!"
+@CLIENT.command()
+async def ping(ctx):
+    await ctx.send("pong!")
+
+
+# When you type /hello
+# the bot will say "hello!"
+@CLIENT.command()
+async def hello(ctx):
+    await ctx.send("hello!")
+
+
+# This method only runs once in the beginning
+@CLIENT.event
+async def on_ready():
+    print("Discord Bot Activated")
+
+
+CLIENT.run(TOKEN)
